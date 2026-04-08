d752f1410ee39ed1972a831fbb1bac7f9721f5f7
Akmal <akarimboyev016@gmail.com>
2026-04-08 00:01:46 -0600

    fourth

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import { AITutorChat } from "@/components/AITutorChat";
 import { useDMNotifications } from "@/hooks/useDMNotifications";
 import { GlobalCallOverlay } from "@/components/community/GlobalCallOverlay";
 import Index from "./pages/Index";
-import AuthLayout from '@/components/AuthLayout';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProfileSettings from "./pages/ProfileSettings";
 import Games from "./pages/Games";
@@ -38,7 +39,8 @@ const App = () => (
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
-                <Route path="/auth" element={<Auth />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/profile" element={<ProfileSettings />} />
                 <Route path="/games" element={<Games />} />
